42f8c7b862139919ba0e606b095668eb70ffbe2a
Darosss <daroing12@gmail.com>
2023-05-25 00:43:12 +0200

    feat: add usage of ValidationPipe + AllExceptionsFilter

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { AllExceptionsFilter } from './exception/all-exceptions.filter';
+import { ValidationPipe } from '@nestjs/common';
 dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const httpAdapter = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(3000);
 }
 bootstrap();
