20b406e47495c61ebbff8939da370b1a2c8b5399
Darosss <daroing12@gmail.com>
2023-05-25 20:10:18 +0200

    fix: add Transfrom as destructed value

--- src/decorators/to-bolean.decorator.ts
+++ src/decorators/to-bolean.decorator.ts
@@ -2,7 +2,7 @@ import { Transform } from 'class-transformer';
 
 export function ToBoolean(): (target: any, key: string) => void {
   return Transform(
-    (value: any) =>
+    ({ value }) =>
       value === 'true' || value === true || value === 1 || value === '1',
   );
 }
