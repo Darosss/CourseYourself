24379ef699f5f4c6b501ef021de0ac1115548e45
Darosss <daroing12@gmail.com>
2023-06-04 12:01:51 +0200

    feat: add SwaggetTags enum helper

---
+++ src/helpers/swagger.helpers.ts
@@ -0,0 +1,10 @@
+export enum SwaggerTags {
+  AUTH = 'auth',
+  USERS = 'users',
+  GROUPS = 'groups',
+  WORKOUTS = 'workouts',
+  EXERCISES = 'exercises',
+  NOTIFICATIONS = 'notifications',
+  PROGRESSES = 'progresses',
+  ANALYTICS = 'analytics',
+}
