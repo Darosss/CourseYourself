8deaaa51f574b58dfa51838c333517a7bd9b9eb5
Darosss <daroing12@gmail.com>
2023-06-09 20:42:09 +0200

    refactor: move CHECK_POLICIES_KEY into constants file

--- src/decorators/check-policies.decorator.ts
+++ src/decorators/check-policies.decorator.ts
@@ -1,6 +1,6 @@
 import { SetMetadata, Type } from '@nestjs/common';
+import { CHECK_POLICIES_KEY } from 'src/casl/constants';
 import { PolicyHandler } from 'src/interfaces/policy-handler.interface';
 
-export const CHECK_POLICIES_KEY = 'check_policy';
 export const CheckPolicies = (...handlers: Type<PolicyHandler>[]) =>
   SetMetadata(CHECK_POLICIES_KEY, handlers);
