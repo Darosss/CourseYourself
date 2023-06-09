50473177ddcd107b2c62f3986f21d27c92629d0c
Darosss <daroing12@gmail.com>
2023-06-09 20:23:24 +0200

    feat: add initial policies

---
+++ src/casl/policies/index.ts
@@ -0,0 +1,7 @@
+import { Type } from '@nestjs/common';
+import { PolicyHandler } from 'src/interfaces/policy-handler.interface';
+
+//TODO: add needed policies to policies array
+export const policies: Type<PolicyHandler>[] = [];
+
+//TODO: add exports needed policies
