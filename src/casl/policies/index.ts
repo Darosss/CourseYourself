44af75176845ff45c7c3947cb58a9d88fdb02c38
Darosss <daroing12@gmail.com>
2023-06-10 13:06:34 +0200

    feat: add groupsPolicies to export

--- src/casl/policies/index.ts
+++ src/casl/policies/index.ts
@@ -1,7 +1,8 @@
 import { Type } from '@nestjs/common';
 import { PolicyHandler } from 'src/interfaces/policy-handler.interface';
 
-//TODO: add needed policies to policies array
-export const policies: Type<PolicyHandler>[] = [];
+export * from './groups';
 
-//TODO: add exports needed policies
+import { groupsPolicies } from './groups';
+
+export const policies: Type<PolicyHandler>[] = [...groupsPolicies];
