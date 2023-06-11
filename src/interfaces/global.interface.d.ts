61a64d44b98c621edc4f303b6a70bc4ce66111ed
Darosss <daroing12@gmail.com>
2023-06-11 16:15:21 +0200

    feat: add userEntity to global Request interface

--- src/interfaces/global.interface.d.ts
+++ src/interfaces/global.interface.d.ts
@@ -1,11 +1,14 @@
 import { Group } from 'src/group/entities/group.entity';
 import { Workout } from 'src/workout/entities/workout.entity';
+import { UserWOPassword } from 'src/user/interfaces/user.interface';
 
 declare global {
   namespace Express {
     interface Request {
       group?: Group;
       workout?: Workout;
+      //need to do, because passport by default uses User
+      userEntity?: UserWOPassword;
     }
   }
 }
