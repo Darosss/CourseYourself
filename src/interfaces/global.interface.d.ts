fafb3e9b28bec1d8717e9080830fb8382d505783
Darosss <daroing12@gmail.com>
2023-06-11 13:44:29 +0200

    feat: add Workout to global Request interface

--- src/interfaces/global.interface.d.ts
+++ src/interfaces/global.interface.d.ts
@@ -1,9 +1,11 @@
 import { Group } from 'src/group/entities/group.entity';
+import { Workout } from 'src/workout/entities/workout.entity';
 
 declare global {
   namespace Express {
     interface Request {
       group?: Group;
+      workout?: Workout;
     }
   }
 }
