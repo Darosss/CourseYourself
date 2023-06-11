5f829ad7e3464792436399a90a45b268329ea551
Darosss <daroing12@gmail.com>
2023-06-11 13:48:40 +0200

    feat: add workout features to export

--- src/casl/policies/index.ts
+++ src/casl/policies/index.ts
@@ -2,7 +2,12 @@ import { Type } from '@nestjs/common';
 import { PolicyHandler } from 'src/interfaces/policy-handler.interface';
 
 export * from './groups';
+export * from './workouts';
 
 import { groupsPolicies } from './groups';
+import { workoutsPolicies } from './workouts';
 
-export const policies: Type<PolicyHandler>[] = [...groupsPolicies];
+export const policies: Type<PolicyHandler>[] = [
+  ...groupsPolicies,
+  ...workoutsPolicies,
+];
