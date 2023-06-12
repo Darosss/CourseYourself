411db1eb26c6c76c8836fb0a2c026b6448236334
Darosss <daroing12@gmail.com>
2023-06-12 14:12:28 +0200

    feat: add RetrieveNotificationByIdMiddleware to Notificaion.Module

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -1,9 +1,16 @@
-import { Module, forwardRef } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  RequestMethod,
+  forwardRef,
+} from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { NotificationController } from './notification.controller';
 import { Notification } from './entities/notification.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from 'src/user/user.module';
+import { RetrieveNotificationByIdMiddleware } from './middlewares/get-notification-by-id.middleware';
+import { NOTIFICATIONS_ROUTE_NAME } from './constants';
 
 @Module({
   imports: [
@@ -14,4 +21,21 @@ import { UserModule } from 'src/user/user.module';
   providers: [NotificationService],
   exports: [NotificationService],
 })
-export class NotificationModule {}
+export class NotificationModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(RetrieveNotificationByIdMiddleware).forRoutes(
+      {
+        path: `${NOTIFICATIONS_ROUTE_NAME}/:id`,
+        method: RequestMethod.PATCH,
+      },
+      {
+        path: `${NOTIFICATIONS_ROUTE_NAME}/:id`,
+        method: RequestMethod.GET,
+      },
+      {
+        path: `${NOTIFICATIONS_ROUTE_NAME}/:id`,
+        method: RequestMethod.DELETE,
+      },
+    );
+  }
+}
