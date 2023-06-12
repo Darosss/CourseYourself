279ac2501b309b31f5f9976ded5afb05ca758abf
Darosss <daroing12@gmail.com>
2023-06-12 15:47:57 +0200

    feat: add progresses casl policy handlers

--- src/casl/policies/index.ts
+++ src/casl/policies/index.ts
@@ -6,12 +6,14 @@ export * from './workouts';
 export * from './users';
 export * from './notifications';
 export * from './exercises';
+export * from './progresses';
 
 import { groupsPolicies } from './groups';
 import { workoutsPolicies } from './workouts';
 import { usersPolicies } from './users';
 import { notificationsPolicies } from './notifications';
 import { exercisesPolicies } from './exercises';
+import { progresssPolicies } from './progresses';
 
 export const policies: Type<PolicyHandler>[] = [
   ...usersPolicies,
@@ -19,4 +21,5 @@ export const policies: Type<PolicyHandler>[] = [
   ...workoutsPolicies,
   ...notificationsPolicies,
   ...exercisesPolicies,
+  ...progresssPolicies,
 ];
