989a8c366695ab809718b977fa8976f7982dcdd1
Darosss <daroing12@gmail.com>
2023-06-12 14:15:54 +0200

    feat: add notifications casl providers

--- src/casl/providers/index.ts
+++ src/casl/providers/index.ts
@@ -3,9 +3,11 @@ import { Provider } from '@nestjs/common';
 import { groupPolicyProviders } from './groups';
 import { workoutPolicyProviders } from './workouts';
 import { userPolicyProviders } from './users';
+import { notificationPolicyProviders } from './notifications';
 
 export const providers: Provider[] = [
   ...userPolicyProviders,
   ...groupPolicyProviders,
   ...workoutPolicyProviders,
+  ...notificationPolicyProviders,
 ];
