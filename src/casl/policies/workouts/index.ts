01d85b59b80dbc533478fd8d8d3b1f2dd7ffb3bb
Darosss <daroing12@gmail.com>
2023-06-28 00:41:02 +0200

    feat: add read-workout policy

--- src/casl/policies/workouts/index.ts
+++ src/casl/policies/workouts/index.ts
@@ -4,13 +4,16 @@ import { PolicyHandler } from 'src/interfaces/policy-handler.interface';
 import { RemoveWorkoutHandler } from './remove-workout.policy';
 import { UpdateWorkoutHandler } from './update-workout.policy';
 import { CreateWorkoutHandler } from './create-workout.policy';
+import { ReadWorkoutHandler } from './read-workout.policy';
 
 export * from './remove-workout.policy';
 export * from './update-workout.policy';
 export * from './create-workout.policy';
+export * from './read-workout.policy';
 
 export const workoutsPolicies: Type<PolicyHandler>[] = [
   RemoveWorkoutHandler,
   UpdateWorkoutHandler,
   CreateWorkoutHandler,
+  ReadWorkoutHandler,
 ];
