0d4f76fed28f63429b8fd30f116ee3fe776ca9bd
Darosss <daroing12@gmail.com>
2023-06-28 00:41:36 +0200

    feat: add workouts providers

--- src/casl/providers/workouts/index.ts
+++ src/casl/providers/workouts/index.ts
@@ -3,9 +3,11 @@ import { Provider } from '@nestjs/common';
 import { RemoveWorkoutPolicyProvider } from './remove-workout.provider';
 import { UpdateWorkoutPolicyProvider } from './update-workout.provider';
 import { CreateWorkoutPolicyProvider } from './create-workout.provider';
+import { ReadWorkoutPolicyProvider } from './read-workout.provider';
 
 export const workoutPolicyProviders: Provider[] = [
   RemoveWorkoutPolicyProvider,
   UpdateWorkoutPolicyProvider,
   CreateWorkoutPolicyProvider,
+  ReadWorkoutPolicyProvider,
 ];
